52270ce1d1195f0473c7038fdb63f6a74070e8ce
Alugbin Boluwatife Godwin <godwinalugbin004@gmail.com>
2023-05-08 08:16:44 +0100

    initial commit

---
+++ src/components/SkeletonCard.vue
@@ -0,0 +1,289 @@
+<template>
+    
+    <section class="skeleton">
+    <div class="card-img"></div>
+    <div class="card-one"></div>
+    <div class="card-two"></div>
+    <div class="card-three"></div>
+    <div class="card-four"></div>
+    </section>
+</template>
+
+<style>
+.skeleton {
+    min-width: 13.4375rem;
+    margin-bottom: 50px;
+   
+}
+.card-img {
+    width: 100%;
+    height: 150px;
+    background-image: linear-gradient(
+    to right,
+    #d6d7d8 0%,
+    #e2e3e4 10%,
+    #d6d7d8 20%,
+    #d6d7d8 100%
+  );
+  margin-bottom: 5px;
+}
+.card-one {
+    width: 80%;
+    height: 20px;
+    background-image: linear-gradient(
+    to right,
+    #d6d7d8 0%,
+    #e2e3e4 10%,
+    #d6d7d8 20%,
+    #d6d7d8 100%
+  );
+  margin-bottom: 10px;
+  background-color: red;
+}
+.card-two {
+    width: 90%;
+    height: 25px;
+    background-image: linear-gradient(
+    to right,
+    #d6d7d8 0%,
+    #e2e3e4 10%,
+    #d6d7d8 20%,
+    #d6d7d8 100%
+  );
+  margin-bottom: 10px;
+  background-color: green;
+}
+.card-three {
+    width: 40%;
+    height: 20px;
+    background-image: linear-gradient(
+    to right,
+    #d6d7d8 0%,
+    #e2e3e4 10%,
+    #d6d7d8 20%,
+    #d6d7d8 100%
+  );
+  margin-bottom: 10px;
+}
+.card-four {
+    width: 70%;
+    height: 25px;
+    background-image: linear-gradient(
+    to right,
+    #d6d7d8 0%,
+    #e2e3e4 10%,
+    #d6d7d8 20%,
+    #d6d7d8 100%
+  );
+}
+.fixed-class {
+    position: fixed;
+    left: 0;
+    right: 0;
+    background: white;
+    top: 0;
+}
+.sleep {
+    font-weight: 400;
+    font-style: normal;
+    font-size: .875rem;
+    line-height: 1.25rem;
+    color: #567390;   
+}
+.sleep.city {
+    margin-bottom: 0.75rem;
+}
+.price{
+    font-weight: 600;
+    font-style: normal;
+    font-size: 1.125rem;
+    line-height: 1.5rem;
+}
+.rate {
+    color: #032e5a;
+}
+.truncate-text {
+    max-width: 10rem;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    color: #032e5a;
+    font-weight: 600;
+    font-size: 1.2rem;
+    line-height: 1.5rem;
+}
+.card-img {
+    height: 150px;
+    border-radius: 1rem;
+}
+.rv-container {
+    display: flex;
+    flex-wrap: wrap;
+    width: 90%;
+    margin: 0 auto;
+    justify-content: space-between;
+}
+.imgs {
+    border-radius: 1rem;
+    width: 100%;
+    height: 100%;
+}
+header {
+    width: 100%;
+    background-color: white;
+    box-shadow: 0 0.125rem 0.125rem 0 rgba(0,17,44,.04);
+    padding: .5rem .8rem;
+}
+.section-1 {
+    display: flex;
+}
+.filter-1 {
+    width: 42%;
+    display: flex;
+    background-color: #f2f5f7;
+    border: 0.0625rem solid #cdd5de;
+    border-radius: 0.5rem;
+    height: 3rem;
+    align-items: center;
+    margin-left: 2rem;
+}
+.filter-1:hover {
+    background-color: #f4f8ff;
+    border-color: black;
+}
+.search-tag {
+    font-size: .75rem;
+    cursor: pointer;
+    color: #35587b;
+    
+}
+.filter-1 input {
+    display: block;
+    padding-right: 2.25rem;
+    border-radius: 0;
+    background-color: transparent;
+    border: none;
+    width: 100%;
+    cursor: pointer;
+}
+.filter-1 input:focus {
+    outline: 0;
+}
+.group-1 {
+    height: 2rem;
+    padding: 0 0.75rem;
+    cursor: pointer;
+}
+.group-1.one {
+   width: 55%;
+}
+.group-1.two {
+    width: 20%;
+    border-left: 0.0625rem solid #cdd5de;
+    border-right: 0.0625rem solid #cdd5de;
+}
+.group-1.three {
+    width: 25%;
+}
+.filters {
+    
+    width: 100%;
+    background-color: hsl(0, 0%, 100%);
+    padding: 0px .8rem 15px .8rem;
+    border-bottom: 0.0625rem solid #dcdcdc;
+    margin-top: 15px;
+    display: flex;
+    justify-content: space-between;
+    position: relative;
+}
+
+.filter-a {
+    width: 65%;
+    display: flex;
+    justify-content: space-between;
+}
+.filter-btn {
+    border: 0.0625rem solid #cdd5de;
+    background-color: #fff;
+    border-radius: 0.25rem;
+    padding: 0.75rem 1rem;
+    color: #032e5a;
+    font-weight: 600;
+    line-height: 1rem;
+    cursor: pointer;
+    font-size: .875rem;
+    display: flex;
+    align-items: center;
+}
+.filter-btn .filter-btn-text {
+    margin-right: 5px;
+}
+.fa-w-14 {
+    width: 0.875rem;
+    height: 0.8rem;
+}
+.filter-btn:hover {
+    background-color: #f2f5f7;
+}
+.class-one {
+    display: flex;
+}
+.classes {
+    display: none;
+}
+.preloader-container {
+    display: flex;
+    width: 90%;
+    margin: 0 auto;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+.drivable-part, .towable-part {
+    max-width: 40rem;
+    width: 50vw;
+    box-shadow: 0 0.1875rem 1.25rem rgba(0,0,0,.22);
+    margin-top: 0.5rem;
+    border: 1px solid #032e5a;
+    border-radius: 0.5rem;
+    z-index: 5;
+    position: absolute;
+    top: 25%;
+    background: white;
+    padding: 1.5rem !important;
+    display: none;
+    margin-left: .8rem;
+}
+.filter-btn .up {
+    display: none;
+}
+.filter-btn.active .up {
+    display: block;
+}
+.filter-btn.active .down {
+    display: none;
+}
+
+.towable-part {
+    margin-left: 8.3rem;
+}
+.drivable-part.active, .towable-part.active {
+    display: block;
+    position: fixed;
+}
+
+.title {
+    width: 90%;
+    margin: 0 auto;
+    margin-top: 200px;
+    margin-bottom: 30px;
+    display: flex;
+    justify-content: space-between;
+}
+.title h2 {
+    color: #032e5a;
+    font-size: 1.5rem;
+}
+select {
+    padding: 0.25rem;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,135 @@
+<template>
+  <section>
+  <head-comp />
+  </section>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeadComp from '@/components/HeadComp.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeadComp
+  }
+  
+}
+</script>
+
+<style>
+.inneer {
+  background-image: linear-gradient(
+    to right,
+    #d6d7d8 0%,
+    #e2e3e4 10%,
+    #d6d7d8 20%,
+    #d6d7d8 100%
+  );
+}
+.guests {
+  position: absolute;
+  z-index: 10;
+  background: white;
+  border: 1px solid #032e5a;
+  border-radius: 0.5rem;
+  padding: 1.5rem!important;
+  box-shadow: 0 0.1875rem 1.25rem rgba(0,0,0,.22);
+  left: 30%;
+  width: 250px;
+  display: none;
+}
+.guests.active {
+  display: block;
+}
+.option {
+  display: flex;
+}
+.ad .option div {
+  width: 30px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.ad .option div:first-child {
+  
+  border:1px solid  #f2f5f7;
+    color: #68829c;
+    border-radius: 50%;
+    font-size: 1.5rem;
+}
+.ad .option div:last-child {
+  font-size: 1.5rem;
+  border-radius: 50%;
+  background-color: transparent;
+    border: 1px solid #032e5a;
+    color: #032e5a;
+}
+.ad, .tow-box {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 30px;
+}
+
+.ad h2 {
+  font-size: 1.2rem;
+    font-weight: 400;
+    color: #032e5a;
+}
+.ad h3 {
+  font-weight: 600;
+    font-style: normal;
+    font-size: .875rem;
+    text-decoration: underline;
+    color: #567390;
+    cursor: pointer;
+    
+}
+.ad:last-child {
+  margin-bottom: 10px;
+  align-items: center;
+}
+.zbtn {
+  padding: 0.5rem 1rem;
+    border-radius: 1.5rem;
+    font-weight: 600;
+    cursor: pointer;
+    background-color: #032e5a;
+    color: #fff;
+}
+.drive-box {
+  display: flex;
+  justify-content: space-between;
+}
+.class-one {
+  padding: 0.25rem!important;
+  display: flex!important;
+  border: 0.0625rem solid #cdd5de;
+    border-radius: 0.25rem;
+    padding: 0 0 0 2.75rem;
+    width: 48%;
+    margin-bottom: 10px;
+    padding: 0.25rem!important;
+}
+.class-one input[type=checkbox] {
+  width: 1.5rem;
+    height: 1.5rem;
+}
+.class-one p, .class-one h2, .select-all {
+  font-weight: 400;
+    color: #68829c;
+    font-size: .875rem;
+    margin-top: 8px;
+}
+.select-all {
+  font-size: 1.2rem;
+  
+}
+.select {
+  margin-bottom: 20px;
+}
+.class-one-part {
+  margin-left: 10px;
+}
+</style>
